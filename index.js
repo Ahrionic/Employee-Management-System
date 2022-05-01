17e5aa6510a7a71b8a3b8e42f6311ce770badc4f
Korey Luu <39642290+Ahrionic@users.noreply.github.com>
2022-05-01 03:30:00 -0700

    mysql change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2')
 const inquirer = require('inquirer')
 
-const db = mysql.createConnection('mysql://root:Bunbun21*@localhost/employeeManager_db')
+const db = mysql.createConnection('mysql://root:Bunbun21*@localhost:3306/employeeManager_db')
 
 // switch case to set up adding and viewing or employees
 const homePage = () => {
